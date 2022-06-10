881302f89b03fc0892f617c36c5d8373c54ee5ad
iconicweb-joe <joe@iconicwebhq.com>
2022-06-10 15:27:06 -0400

    Update 07_array_functions.php

--- php-crash-main/07_array_functions.php
+++ php-crash-main/07_array_functions.php
@@ -53,9 +53,11 @@ var_dump($flipped);
 $numbers = range(1, 20);
 
 // Map through array and create a new one
-$newNumbers = array_map(function ($number) {
-  return "Number ${number}";
+$newNumbers = array_map(function ($test) {
+  return "Number ${test}";
 }, $numbers);
+
+print_r($newNumbers);
 
 // Filter array
 $lessThan10 = array_filter($numbers, fn ($number) => $number < 10);
@@ -64,3 +66,63 @@ $lessThan10 = array_filter($numbers, fn ($number) => $number < 10);
 // "carry" holds the return value of the previous iteration
 $sum = array_reduce($numbers, fn ($carry, $number) => $carry + $number);
 var_dump($sum);
+
+
+
+
+// My Attempt
+echo "<br><br>";
+$colors = ['red', 'blue', 'green', 'white', 'orange'];
+print_r($colors);
+array_unshift($colors, 'black');
+echo "<br>" . $colors[0] . "<br>";
+print_r($colors[1]);
+echo "<br>" . count($colors);
+
+// Chunked
+$chucked_array = array_chunk($colors, 2);
+echo "<br>";
+print_r($chucked_array);
+
+// Combined Arrays
+echo "<br><br>";
+$colors_fruits = [
+  "green" => "avacado",
+  "red" => "apple",
+  "yellow" => "banana",
+];
+print_r($colors_fruits);
+$colors_1 = ['green', 'red', 'yellow'];
+$fruits_1 = ['avacado', 'apple', 'banana'];
+$colors_1_fruits_1 = array_combine($colors_1, $fruits_1);
+echo "<br>";
+print_r($colors_1_fruits_1);
+
+// Print Keys
+$the_keys = array_keys($colors_1_fruits_1);
+echo "<br>";
+print_r($the_keys);
+
+// Flipped Array
+$flipped_array = array_flip($the_keys);
+echo "<br>";
+print_r($flipped_array);
+
+
+// Range
+$range = range(1, 20);
+echo "<br>";
+print_r($range);
+
+// Bryce
+echo "<br><br>" . "Bryce big poopy";
+
+// Bryce, but in an array
+$bryce_but_an_array = [
+  "name" => "not bryce",
+  "age" => "not 24",
+  "eye color" => "not green"
+];
+
+echo "<br><br>";
+print_r($bryce_but_an_array);
